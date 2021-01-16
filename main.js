1226ac20ef2ef45ba37a8c0980366fccde526874
unknown <githublmao@seznam.cz>
2021-01-16 12:45:49 +0100

    druha verze, trocha js, hodne chyb pomoc

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d')
 
+let apple = document.getElementById("apple");
+let kosik = document.getElementById("kosik");
+let drama = new Audio("sound/drama.mp3");
+let fun = new Audio("sound/fun.mp3");
+let jabkoSpeed = 3;
+
+let player = {
+    x: canvas.width/2,
+    y: 20,
+    keys:[],
+
+    move: function(){
+        if(this.keys["ArrowRight"]){
+            this.x +=50;
+            console.log("prava");
+        }
+        if(this.keys['ArrowLeft']){
+            this.x -=50;
+        }
+    },
+
+    paint: function(){
+        ctx.drawImage(kosik, this.x, this.y);
+    },
+}
+let game = {
+
+}
